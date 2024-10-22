d620ed5b35df2473b09d6be0cd07b1404d044fc0
Brandon Balderas <brandonbalderas90@gmail.com>
2024-10-22 11:12:29 -0400

    Create game Files
    Getting started on the game files.

---
+++ drawCanvas.js
@@ -0,0 +1,9 @@
+
+function draw() {
+    const canvas = document.getElementById("tutorial");
+    if(canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+    }
+}
+
+window.addEventListener("load", draw);
