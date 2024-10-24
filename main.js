12c78169231ebce01309d6225f1c87a99657e8d9
Brandon Balderas <brandonbalderas90@gmail.com>
2024-10-24 12:27:05 -0400

    Screen Switching
    Trying to get a click to switch to the Character Creation Screen.

---
+++ main.js
@@ -0,0 +1,23 @@
+// initializes character creation, screens and stats.
+const titleScreen = document.getElementById('titleScreen');
+let playerName;
+
+//Verifies that you clicked the screen. Will use this to load in next screen, player naming screen.
+titleScreen.addEventListener('click', nextScreen);
+
+function nextScreen(){
+    console.log("Its working!")
+};
+
+console.log(inputPlayerName);
+
+//Ask for player name
+playerName = Element.getElementById('inputPlayerName');
+
+
+
+
+
+
+
+
