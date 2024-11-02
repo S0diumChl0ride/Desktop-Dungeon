27a6bd1c29247452dae15fab944bd3b478cf840d
Brandon Balderas <brandonbalderas90@gmail.com>
2024-11-02 12:10:13 -0400

    A whole lot.
    Started messing with the JS code to allow the player to click on the title and have it clear the screen and load the next one.

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-// initializes character creation, screens and stats.
-const titleScreen = document.getElementById('titleScreen');
-let playerName;
+// initializes the main canvas of the game
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d'); //Creates a super paintbrush to draw stuff in 2d. Places it in a variable called CTX
+canvas.width = 720; 
+canvas.height = 840;
 
-//Verifies that you clicked the screen. Will use this to load in next screen, player naming screen.
-titleScreen.addEventListener('click', nextScreen);
+titleText();
 
-function nextScreen(){
-    console.log("Its working!")
+
+function titleText(){
+    let text = document.querySelector('canvas').getContext('2d');
+    text.font = '60px serif';
+    text.strokeStyle = 'white';
+    text.textAlign = "center";
+    text.strokeText("Iron's Embrace", canvas.width / 2, 150);
+
+    text.font = '40px serif';
+    text.strokeStyle = 'white';
+    text.textAlign = "center";
+    text.strokeText("Descend into her Embrace...", canvas.width / 2, 720);
+
+    console.log("It's working!");
 };
 
-console.log(inputPlayerName);
+canvas.addEventListener('click', function startGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas. height);
+});
 
-//Ask for player name
-playerName = Element.getElementById('inputPlayerName');
+
+
+
+
+
+
+
+// Creates random squares based on the loop parameters. Also only displays on canvas
+/*for(let i = 0; i < 6; i++){
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.4)';
+    ctx.fillRect(x, y, 50, 50);
+};*/
 
 
 
